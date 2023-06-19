7002d16be018efa9abad81896df0875eb57f0492
Darwin Ding <dingdarwin@gmail.com>
2023-06-19 08:47:45 -0400

    finally got basic lighting working. performance is trash.

--- src/script.ts
+++ src/script.ts
@@ -1,14 +1,24 @@
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
-import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+// import { FBXLoader } from 'three/examples/jsm/loaders/FBXLoader'
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { DRACOLoader } from 'three/addons/loaders/DRACOLoader.js'
 import Stats from 'three/examples/jsm/libs/stats.module'
 
 const scene = new THREE.Scene()
 scene.add(new THREE.AxesHelper(5))
 
-const light = new THREE.PointLight()
-light.position.set(0.8, 1.4, 1.0)
-scene.add(light)
+// const light = new THREE.PointLight()
+// light.position.set(0.8, 1.4, 1.0)
+// scene.add(light)
+
+var hemiLight = new THREE.HemisphereLight( 0xffffff, 0x444444 );
+hemiLight.position.set( 0, 300, 0 );
+scene.add( hemiLight );
+
+var dirLight = new THREE.DirectionalLight( 0xffffff );
+dirLight.position.set( 75, 300, -75 );
+scene.add( dirLight );
 
 const ambientLight = new THREE.AmbientLight()
 scene.add(ambientLight)
@@ -29,31 +39,68 @@ const controls = new OrbitControls(camera, renderer.domElement)
 controls.enableDamping = true
 controls.target.set(0, 1, 0)
 
-const material = new THREE.MeshNormalMaterial({ color: 0x808080})
-material.color.convertSRGBToLinear()
+const material = new THREE.MeshBasicMaterial()
 
-const fbxLoader = new FBXLoader()
-fbxLoader.load(
-    'clivia-miniata-textures-insteadofshaders.fbx',
-    (object) => {
-        object.traverse(function (child) {
-            if ((child as THREE.Mesh).isMesh) {
-                (child as THREE.Mesh).material = material
-                if ((child as THREE.Mesh).material) {
-                    ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
-                }
-            }
-        })
-        object.scale.set(.01, .01, .01)
-        scene.add(object)
-    },
-    (xhr) => {
-        console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
-    },
-    (error) => {
-        console.log(error)
-    }
-)
+// const fbxLoader = new FBXLoader()
+// fbxLoader.load(
+//     'clivia-textures-attempt-1000.fbx',
+//     (object) => {
+//         object.traverse(function (child) {
+//             if ((child as THREE.Mesh).isMesh) {
+//                 (child as THREE.Mesh).material = material
+//                 if ((child as THREE.Mesh).material) {
+//                     ((child as THREE.Mesh).material as THREE.MeshBasicMaterial).transparent = false
+//                 }
+//             }
+//         })
+//         object.scale.set(.01, .01, .01)
+//         scene.add(object)
+//     },
+//     (xhr) => {
+//         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+//     },
+//     (error) => {
+//         console.log(error)
+//     }
+// )
+// Instantiate a loader
+
+const loader = new GLTFLoader();
+
+// Optional: Provide a DRACOLoader instance to decode compressed mesh data
+const dracoLoader = new DRACOLoader();
+dracoLoader.setDecoderPath( '/examples/jsm/libs/draco/' );
+loader.setDRACOLoader( dracoLoader );
+
+// Load a glTF resource
+loader.load(
+	// resource URL
+	'clivia-textures-attempt-2.glb',
+	// called when the resource is loaded
+	function ( gltf ) {
+
+		scene.add( gltf.scene );
+
+		gltf.animations; // Array<THREE.AnimationClip>
+		gltf.scene; // THREE.Group
+		gltf.scenes; // Array<THREE.Group>
+		gltf.cameras; // Array<THREE.Camera>
+		gltf.asset; // Object
+
+	},
+	// called while loading is progressing
+	function ( xhr ) {
+
+		console.log( ( xhr.loaded / xhr.total * 100 ) + '% loaded' );
+
+	},
+	// called when loading has errors
+	function ( error ) {
+
+		console.log( 'An error happened' );
+
+	}
+);
 
 window.addEventListener('resize', onWindowResize, false)
 function onWindowResize() {
